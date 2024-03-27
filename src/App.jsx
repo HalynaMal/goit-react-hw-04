d9d7f2d85acb2c87a8c90932fc2861c577857e8d
HalynaMal <dguoit@ukr.net>
2024-03-28 00:07:46 +0200

    check

--- src/App.jsx
+++ src/App.jsx
@@ -24,34 +24,47 @@ import './App.css'
 //   }
 // }
 
+// "urls": {
+//     "raw": "https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bc01c83c3da0425e9baa6c7a9204af81",
+//     "full": "https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjEyMDd9&s=ce40ce8b8ba365e5e6d06401e5485390",
+//     "regular": "https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9&s=fb86e2e09fceac9b363af536b93a1275",
+//     "small": "https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9&s=dd060fe209b4a56733a1dcc9b5aea53a",
+//     "thumb": "https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9&s=50827fd8476bfdffe6e04bc9ae0b8c02"
+//   },
+
 function App() {
-  // 1. Оголошуємо стан
+  
   const [images, setImages] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
   
   useEffect(() => {
+    setIsLoading(true)
     async function fetchImages() {
       const { data } = await axios.get(
         "https://api.unsplash.com/photos/?client_id=Py_iDo0pAQqvEYoMBPZrJ7mc9_8W17Y3JqrImjifwlY"
       );
-      console.log(data);
-      setImages(data.urls);
+      // console.log(data);
+      setImages(data.small);
+      setIsLoading(false)
     }
     fetchImages();
   }, [])
 
-  
-  
+   
   return (
     <div>
       <h1>Collection img</h1>
+      {isLoading && <div>Loading...</div> }
       <ul>
-        {images.map(image => {
-          return (<li key={image.id}>
-          <img width={250} src="" alt={image.title} />
-          <h2>{image.title}</h2>
-          <p>{image.description}</p>
-          <h3>{image.published_at}</h3>
-        </li>)
+        {Array.isArray(images) && images.map(image => {
+          return (
+            <li key={image.id}>
+              <img width={250} src={image.small} alt={image.title} />
+              <h2>{image.title}</h2>
+              <p>{image.description}</p>
+              <h3>{image.published_at}</h3>
+            </li>
+          );
         }) }
         
       </ul>
